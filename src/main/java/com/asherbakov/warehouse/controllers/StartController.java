3bda44049164142d25ac5b0f3b2cb47bd672259b
AScherbakov <illusion-of-meaning@ya.ru>
2023-01-06 20:58:09 +0900

    Initial commit

---
+++ src/main/java/com/asherbakov/warehouse/controllers/StartController.java
@@ -0,0 +1,19 @@
+package com.asherbakov.warehouse.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Основной контроллер")
+public class StartController {
+    @Operation(summary = "Стратовая страница")
+    @GetMapping("/")
+    public String start() {
+        return """
+                <h1>Склад 1.0</h1>
+                приложение запущено
+                """;
+    }
+}
